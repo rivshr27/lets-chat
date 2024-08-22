fc6feb269974ad908fa2a65a4d25249fd4093cff
Vinit  Singh <rivshunyrt27@gmail.com>
2024-08-22 16:26:42 +0530

    fix

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -4,19 +4,23 @@ import { getAuth } from "firebase-admin/auth";
 import { config } from "dotenv";
 config();
 
-const serviceAccountKey = {
-  type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-  universe_domain: process.env.UNIVERSE_DOMAIN,
-};
+// const serviceAccountKey = {
+//   type: process.env.TYPE,
+//   project_id: process.env.PROJECT_ID,
+//   private_key_id: process.env.PRIVATE_KEY_ID,
+//   private_key: process.env.PRIVATE_KEY.replace(/\\n/g,'\n'),
+//   client_email: process.env.CLIENT_EMAIL,
+//   client_id: process.env.CLIENT_ID,
+//   auth_uri: process.env.AUTH_URI,
+//   token_uri: process.env.TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+//   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+//   universe_domain: process.env.UNIVERSE_DOMAIN,
+// };
+
+const serviceAccountKey = JSON.parse(process.env.FB)
+
+console.log(serviceAccountKey);
 
 const app = initializeApp({
   credential: cert(serviceAccountKey),
